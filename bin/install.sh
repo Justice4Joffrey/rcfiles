1add060468a406fca39e264bc776ee45c8ad0316
Otto Castle <otto.castle1@gmail.com>
2022-05-20 11:49:01 +0100

    fix path

--- bin/install.sh
+++ bin/install.sh
@@ -3,5 +3,5 @@
 ln -s $(readlink -f tmux.conf) ~/.tmux.conf
 ln -s $(readlink -f alacritty.yml) ~/.alacritty.yml
 ln -s $(readlink -f vimrc) ~/.vimrc
-ln -s $(readlink -f vimrc) ~/.config/nvm/init.vim
+ln -s $(readlink -f vimrc) ~/.config/nvim/init.vim
 
