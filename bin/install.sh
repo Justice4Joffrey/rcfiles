17d17f8e5edb722f8bd7615c30e38818e7a9be39
Otto Castle <otto.castle1@gmail.com>
2022-05-20 12:37:40 +0100

    link ideavim

--- bin/install.sh
+++ bin/install.sh
@@ -3,5 +3,6 @@
 ln -s $(readlink -f tmux.conf) ~/.tmux.conf
 ln -s $(readlink -f alacritty.yml) ~/.alacritty.yml
 ln -s $(readlink -f vimrc) ~/.vimrc
+ln -s $(readlink -f vimrc) ~/.ideavimrc
 ln -s $(readlink -f vimrc) ~/.config/nvim/init.vim
 
